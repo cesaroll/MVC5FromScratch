81bd1ea5993a64d2fc261972b3c7245f42c4de2e
Cesar Lopez <cesarl@outlook.com>
2015-11-12 16:37:12 -0700

    Update Assembly info

--- MVC5_From_Scratch/S04_Separated_Solutions/Employee_Models/Employee_Models/Properties/AssemblyInfo.cs
+++ MVC5_From_Scratch/S04_Separated_Solutions/Employee_Models/Employee_Models/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Employee_Models")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Employee.Models")]
+[assembly: AssemblyDescription("Employee Models Assembly")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Cesar Lopez")]
 [assembly: AssemblyProduct("Employee_Models")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
