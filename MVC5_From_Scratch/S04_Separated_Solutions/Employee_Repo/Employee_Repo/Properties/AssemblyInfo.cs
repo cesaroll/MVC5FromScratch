abff24a5ef3cd800c32719a0fea20550c3a63179
Cesar Lopez <cesarl@outlook.com>
2015-11-12 19:56:47 -0700

    Added nuspec

--- MVC5_From_Scratch/S04_Separated_Solutions/Employee_Repo/Employee_Repo/Properties/AssemblyInfo.cs
+++ MVC5_From_Scratch/S04_Separated_Solutions/Employee_Repo/Employee_Repo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Employee_Repo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Employee.Repo")]
+[assembly: AssemblyDescription("Employee Repo Assembly")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Employee_Repo")]
+[assembly: AssemblyCompany("Cesar Lopez")]
+[assembly: AssemblyProduct("Employee.Repo")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
